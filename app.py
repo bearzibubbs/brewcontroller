bbfda74edc614a389e60cf2ee48c4b3f1e5b2280
aldnoah <apeterlegg@gmail.com>
2021-11-21 12:12:46 -0500

    set up pseudo code and the loop (which prob wont work)

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-import controller.pid
-import sys
+import controller.simple_pid
+import datetime
 
-def pid_loop(P, I, D, mode=boil, duration):
+def pid_loop(self):
+    
+
+    # Need following information from the front end 
+    # get Kp, Ki, Kd values - Default values available
+    # get collection interval - Defaults values available
+    # get PID mode {boil, mash}
+    # get duration of loop in minutes
+    # temp setpoint
+    start = datetime.now()
+
+    PID = controller.simple_pid.PID()
+    PID.tunings(Kp, Ki, Kd)
 
 
+    while ( flag && datetime.now()-start < duration ):
+        #get temp from MAX
+        settemp =  None # need to get this from front end
+        PID(settemp)        
+    PID.reset()
 
 if __name__ == "__main__":
     # should I def the main loop and then just put it here?
