f17eaa5e19e22a5a68c56f3484a48c46c5509895
aldnoah <apeterlegg@gmail.com>
2021-12-16 14:47:09 -0500

    added time to while loop

--- app.py
+++ app.py
@@ -1,26 +1,60 @@
-import controller.simple_pid
-import datetime
+#!/usr/bin/python3
 
-def pid_loop(self):
+import controller.simple_pid
+import time
+import sys
+import spidev
+
+def pid_loop(currentTemp):
     
 
-    # Need following information from the front end 
+    # Need following information from the cli
     # get Kp, Ki, Kd values - Default values available
     # get collection interval - Defaults values available
     # get PID mode {boil, mash}
     # get duration of loop in minutes
     # temp setpoint
-    start = datetime.now()
 
-    PID = controller.simple_pid.PID()
-    PID.tunings(Kp, Ki, Kd)
+    PID(currentTemp)        
 
 
-    while ( flag && datetime.now()-start < duration ):
-        #get temp from MAX
-        settemp =  None # need to get this from front end
-        PID(settemp)        
-    PID.reset()
 
 if __name__ == "__main__":
     # should I def the main loop and then just put it here?
+    
+    # for CLI - to be removed
+    # error out if too many args, need to add test for proper int type
+    
+    args = sys.argv[1:]    
+    for arg in args:
+        if arg == '-h':
+            print('app.py <setTemp>')
+            sys.exit()
+    if len(args) > 1:
+        print('Too many argumnents')
+        sys.exit()
+
+
+    # set up the env with vars
+
+    setTemp = int(args[0])
+    start = time.time()
+    duration = 5
+    t_end = start + duration * 60
+
+    # Connect to pt100 with spidev
+    pt100spi = spidev.SpiDev()
+    # pt100spi.open(bus, device) -> needs bus and device name, needs a test with raspi
+    # pt100spi.threewire = true
+
+    PID = controller.simple_pid.PID(Kp=1, Ki=1, Kd=1, setPoint=setTemp, sample_time=1)
+
+    while (time.time() < t_end ):
+
+        # get pt100 temp reading
+        # pt100_temp = pt100spi.readbytes(n) spidev something someting
+
+        pid_loop(pt100_temp)
+        print(time.time(), t_end)
+
+    #PID.reset()
