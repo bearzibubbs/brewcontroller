e17a3240a1336fcee02d3661c3da74df816a23cd
aldnoah <apeterlegg@gmail.com>
2021-12-17 17:26:15 -0500

    changed init pid to run pid

--- app_flask.py
+++ app_flask.py
@@ -1,7 +1,7 @@
 import flask
 from flask import request, jsonify
 import controller.simple_pid
-
+import time
 
 app = flask.Flask(__name__)
 
@@ -11,14 +11,26 @@ def home():
     <p> This is the main page for now </p>
     '''
 
-@app.route('/api/v1/initpid', methods=['POST', 'PUT'])
-def api_initpid():
+@app.route('/api/v1/runpid', methods=['POST', 'PUT'])
+def api_runpid():
 
     if !request.is_json():
         return "yo this ain't json"
 
     setTemp = request.get_json()['setTemp']
     pid = controller.simple_pid.PID(Kp=1, Ki=1, Kd=1, setPoint=setTemp, sample_time=1)
+
+    start = time.time()
+    duration = 1 
+    t_end = start + duration * 60
+
+    currTemp = 1
+
+    pid(currTemp)
+
+    
+@app.route('/api/v1/updatepid', methods=['GET'])
+def api_
 
  
  app.run()
