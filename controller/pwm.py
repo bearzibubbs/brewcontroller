781c53f5842494d526e225901720b39a687ed4f2
Peter Legg <apeterlegg@gmail.com>
2022-01-01 17:43:46 -0500

    added reset and del methods

--- controller/pwm.py
+++ controller/pwm.py
@@ -6,7 +6,7 @@ class PWM:
     def __init__(self, freq):
         IO.setmode(IO.BCM)
         IO.setup(19, IO.OUT)
-        self.PWM = IO.PWM(19, freq)
+        self.PWM = IO.PWM(13, freq)
 
 
     def startPWM(self, dutycycle):
@@ -14,6 +14,15 @@ class PWM:
 
     def updatePWM(self, dutycycle):
         self.PWM.ChangeDutyCycle(dutycycle)
+
+    def reset(self):
+        IO.output(13, IO.LOW)
+        self.PWM = IO.PWM(13, 0)
+        self.updatePWM(0)
+        IO.cleanup(13)
+
+    def __del__(self):
+        self.reset()
 
 
 
