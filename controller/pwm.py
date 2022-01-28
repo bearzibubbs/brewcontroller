3d0aa90783d2bfa89f66603acd2a184c8f5b3c2e
aldnoah <apeterlegg@gmail.com>
2022-01-28 09:38:35 -0500

    fix pin to _pin reference

--- controller/pwm.py
+++ controller/pwm.py
@@ -6,27 +6,27 @@ class PWM:
     def __init__(self, freq, pin):
         self._pin = pin
         self._freq = freq
-        self.PWM = IO.PWM(self.pin, self.freq)
+        self.PWM = IO.PWM(pin, freq)
 
         IO.setmode(IO.BCM)
-        IO.setup(self.pin, IO.OUT)
+        IO.setup(pin, IO.OUT)
 
     def getInstance(self):
         return self.PWM
     
     def updatePWM(self, newFreq):
-        self.PWM = IO.PWM(self.pin, newFreq)
+        self.PWM = IO.PWM(self._pin, newFreq)
 
     def reset(self):
-        IO.output(self.pin, IO.LOW)
-        self.PWM = IO.PWM(self.pin, 0)
+        IO.output(self._pin, IO.LOW)
+        self.PWM = IO.PWM(self._pin, 0)
         self.updatePWM(0)
-        return "Pin " + self.pin + " has been set to IO.LOW and PWM freq and dutycycle set to 0."
+        return "Pin " + self._pin + " has been set to IO.LOW and PWM freq and dutycycle set to 0."
 
     def __del__(self):
         self.reset()
-        IO.cleanup(self.pin)
-        return "Pin " + self.pin + " has been set to IO.LOW and PWM object has been cleared."
+        IO.cleanup(self._pin)
+        return "Pin " + self._pin + " has been set to IO.LOW and PWM object has been cleared."
 
 
 
