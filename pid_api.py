47af89773b68cb41acdbc5cde4675a4219f18eca
aldnoah <apeterlegg@gmail.com>
2022-04-19 19:51:09 -0400

    added 1 pid step for test

--- pid_api.py
+++ pid_api.py
@@ -16,7 +16,8 @@ duration = 60
 end = start - duration * 60
 pulseOutput = controller.pwm.PWM(13)
 pulseOutput.startPWM(1/pidControl.sample_time)
-
+latest = pidControl(currentTemp.getTemp())
+pulseOutput.updatePWM(latest)
 
 """ while (time.time() < end ):
     latest = pidControl(currentTemp.getTemp())
