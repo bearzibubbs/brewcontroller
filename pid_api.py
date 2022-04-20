674c2d25231f770cc48696d9786490f19e39f6f1
aldnoah <apeterlegg@gmail.com>
2022-04-19 20:01:06 -0400

    removed lines for test

--- pid_api.py
+++ pid_api.py
@@ -16,8 +16,6 @@ duration = 60
 end = start - duration * 60
 pulseOutput = controller.pwm.PWM(13)
 pulseOutput.startPWM(1/pidControl.sample_time)
-latest = pidControl(tempProbe.getTemp())
-#pulseOutput.updatePWM(latest)
 
 """ while (time.time() < end ):
     latest = pidControl(currentTemp.getTemp())
